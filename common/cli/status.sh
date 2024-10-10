2d75ff561a69c164d6fbb6320a27f04931279cb0
Anonoei <dev@anonoei.com>
2024-10-09 15:30:47 -1000

    reload on update

--- common/cli/status.sh
+++ common/cli/status.sh
@@ -3,19 +3,20 @@ _ash_reinstall() {
 }
 
 _ash_update() {
-    git_version=$(curl --silent "${ASH_DOWN}/${ASH_REL}/${ASH_VER_PATH}" | grep "ASH_VERSION")
-    git_version=${git_version#*=}
+    #git_version=$(curl --silent "${ASH_DOWN}/${ASH_REL}/${ASH_VER_PATH}" | grep "ASH_VERSION")
+    #git_version=${git_version#*=}
 
-    if [ "$git_version" != "\"$ASH_VERSION\"" ]; then
-        echo "Your version is out of date!"
-        echo "Installed: \"$ASH_VERSION\" / Found: $git_version"
-        echo ""
+    #if [ "$git_version" != "\"$ASH_VERSION\"" ]; then
+        #echo "Your version is out of date!"
+        #echo "Installed: \"$ASH_VERSION\" / Found: $git_version"
+        #echo ""
         old_path=$PWD
         cd $ASH_PATH
         git reset --hard origin
         git pull origin main --force
         cd $old_path
-    else
-        echo "AnoSH is up to date."
-    fi
+        exec bash
+    #else
+    #    echo "AnoSH is up to date."
+    #fi
 }
