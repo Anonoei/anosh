57853ff54f716eb946327117a3791bda3997c818
Anonoei <dev@anonoei.com>
2024-11-09 17:25:47 -1000

    Clean up-v0.10.2

---
+++ nix/ano.sh
@@ -0,0 +1,8 @@
+# --- AnoSH User settings --- #
+export ASH_CONFIG_ROOT="$HOME/.local/anosh"
+
+export ASH_CONFIG_HOME="https://github.com/anonoei/anosh"
+export ASH_CONFIG_DOWN="https://raw.githubusercontent.com/anonoei/anosh/main"
+
+export ASH_CONFIG_LOG_LEVEL="WARN"
+# --------------------------- #
